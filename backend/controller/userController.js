f5d7d71b95c33faf300063f76bb3b5aa2c3c2cf3
Anmol Singh <ASingh2320@outlook.com>
2023-02-12 01:59:24 -0500

    started backend

---
+++ backend/controller/userController.js
@@ -0,0 +1,9 @@
+const asyncHandler = require('express-async-handler');
+const User = require('../models/user-model');
+
+// const login = asyncHandler(async (req, res) => {
+//     const goals = await Goal.find();
+//     return res.status(200).json(goals);
+
+//     const user = await User.find({ text: req.body.text})
+// })
